639bfd1917975b0beffa89a36c69df0f1fae85c7
AnnMarkham <ann.e.markham@outlook.com>
2021-01-08 17:56:18 -0700

    basic model setup without validation or references for associations

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,30 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL, 
+      //validate that value is a decimal      
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      //validate value is numeric
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      //references the category model's id
+    }
   },
   {
     sequelize,
